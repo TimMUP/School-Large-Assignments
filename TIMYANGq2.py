632d0c393323719b983af18c3411cead9b5063a1
Tim.MUP <38474993+TimMUP@users.noreply.github.com>
2018-04-18 21:59:41 -0400

    GR 11 Python - Rectangle and Vertices
    This program is designed not to crash at all and to output the correct response each time. Input instructions are displayed once the system runs.

---
+++ TIMYANGq2.py
@@ -0,0 +1,285 @@
+#Level 2
+#import math
+#Ax = 3
+#Ay = 5
+#length = 17
+#width = 21
+
+#Bx = Ax + length
+#By = Ay
+
+#Cx = Ax + length
+#Cy = Ay + width
+
+#Dx = Ax
+#Dy = Ay + width
+
+#print ("The rectangle has corners A(%i,%i), B(%i,%i), C(%i,%i), D(%i,%i)." %(Ax,Ay,Bx,By,Cx,Cy,Dx,Dy))
+
+
+#Level 3
+#import math
+#Ax = 3
+#Ay = 5
+#length = 17
+#width = 21
+
+#Bx = Ax + length
+#By = Ay
+
+#Cx = Ax + length
+#Cy = Ay + width
+
+#Dx = Ax
+#Dy = Ay + width
+
+#print ("The rectangle has corners A(%i,%i), B(%i,%i), C(%i,%i), D(%i,%i)." %(Ax,Ay,Bx,By,Cx,Cy,Dx,Dy))
+#Px = int(input("Please enter the X-Coordinate of your point: "))
+#Py = int(input("Please enter the Y-Coordinate of your point: "))
+#if Ax <= Px <= Cx and Ay <= Py <= Cy:
+    #print ("Your coordinates lies within this rectangle.")
+#else:
+    #print ("Your coordinates dose not lie within this rectangle.")
+
+
+#Level 3+
+#import math
+
+#def XYSeperation(ans):
+    #x = int(ans[1:ans.find(",")])
+    #y = int(ans[ans.find(",") + 1:ans.find(")")])
+    #return x, y
+
+#Ax = 3
+#Ay = 5
+#length = 17
+#width = 21
+
+#Bx = Ax + length
+#By = Ay
+
+#Cx = Ax + length
+#Cy = Ay + width
+
+#Dx = Ax
+#Dy = Ay + width
+
+#print ("The rectangle has corners A(%i,%i), B(%i,%i), C(%i,%i), D(%i,%i)." %(Ax,Ay,Bx,By,Cx,Cy,Dx,Dy))
+#PXY = input("Please enter the X and Y coordinates of your point in form (x,y): ")
+#Px, Py = XYSeperation(PXY)
+#if Ax <= Px <= Cx and Ay <= Py <= Cy:
+    #print ("Your coordinates lies within this rectangle.")
+#else:
+    #print ("Your coordinates dose not lie within this rectangle.")
+
+
+#Level 4-
+#from math import *
+
+#def XYVSeperation(ans):
+    #string = ans.replace(" ","")
+    #x = int(string[1:string.find(",")])
+    #y = int(string[string.find(",") + 1:string.find(")")])
+    #v = string[string.find(")") + 1::]
+    #return x, y, v
+#def pointsDistance(x1, y1, x2, y2):
+    #d = float(sqrt((x2 - x1)**2 + (y2 - y1)**2))
+    #return d
+#pass1 = True
+#Vx = 0
+#Vy = 0
+
+#Ax = 3
+#Ay = 5
+#length = 17
+#width = 21
+
+#Bx = Ax + length
+#By = Ay
+
+#Cx = Ax + length
+#Cy = Ay + width
+
+#Dx = Ax
+#Dy = Ay + width
+
+#print ("The rectangle has corners A(%i,%i), B(%i,%i), C(%i,%i), D(%i,%i)." %(Ax,Ay,Bx,By,Cx,Cy,Dx,Dy))
+#PXV = input("Please enter the X and Y coordinates of your point, followed by a vertices of your choice in the form (x,y)V (V being either A, B, C or D): ")
+#Px, Py ,V = XYVSeperation(PXV)
+#if Ax <= Px <= Cx and Ay <= Py <= Cy:
+    #print ("Your coordinates lies within this rectangle.")
+    #if V == "A":
+        #Vx = Ax
+        #Vy = Ay
+    #elif V == "B":
+        #Vx = Bx
+        #Vy = By
+    #elif V == "C":
+        #Vx = Cx
+        #Vy = Cy
+    #elif V == "D":
+        #Vx = Dx
+        #Vy = Dy
+    #else:
+        #print ("However, your choice of a vertice was neither A, B, C nor D. Please try again.")
+        #pass1 = False
+    #if pass1 == True:
+        #finalDistance = pointsDistance(Px, Py, Vx, Vy)
+        #print ("The point of your choice (%i,%i), is %0.4f units away from vertice %s." %(Px, Py, finalDistance, V))
+#else:
+    #print ("Your coordinates dose not lie within this rectangle.")
+
+
+##Level 4
+#from math import *
+
+#def XYVSeperation(ans):
+    #string = ans.replace(" ","")
+    #x = int(string[1:string.find(",")])
+    #y = int(string[string.find(",") + 1:string.find(")")])
+    #v = string[string.find(")") + 1::]
+    #return x, y, v
+#def pointsDistance(x1, y1, x2, y2):
+    #d = float(sqrt((x2 - x1)**2 + (y2 - y1)**2))
+    #return d
+#def findClosestVertice(x1, y1):
+    #A = pointsDistance(x1, y1, Ax, Ay)
+    #B = pointsDistance(x1, y1, Bx, By)
+    #C = pointsDistance(x1, y1, Cx, Cy)
+    #D = pointsDistance(x1, y1, Dx, Dy)
+    #if A > B or A > B or A > C or A > D:
+        #if B > C or B > D:
+            #if C > D:
+                #ans = "D"
+            #else:
+                #ans = "C"
+        #else:
+            #ans = "B"
+    #else:
+        #ans = "A"
+
+    #return ans
+#typeOfInput = 1
+#Vx = 0
+#Vy = 0
+
+#Ax = 3
+#Ay = 5
+#length = 17
+#width = 21
+
+#Bx = Ax + length
+#By = Ay
+
+#Cx = Ax + length
+#Cy = Ay + width
+
+#Dx = Ax
+#Dy = Ay + width
+
+#print ("The rectangle has corners A(%i,%i), B(%i,%i), C(%i,%i), D(%i,%i)." %(Ax,Ay,Bx,By,Cx,Cy,Dx,Dy))
+#PXV = input("Please enter the X and Y coordinates of your point, followed by a vertices of your choice in the form (x,y)V (V being either \"A, B, C, D or V\". Inputting V will result in the return of the closest vertice to your point): ")
+#Px, Py ,V = XYVSeperation(PXV)
+#if Ax <= Px <= Cx and Ay <= Py <= Cy:
+    #print ("Your coordinates lies within this rectangle.")
+    #if V == "A":
+        #Vx = Ax
+        #Vy = Ay
+    #elif V == "B":
+        #Vx = Bx
+        #Vy = By
+    #elif V == "C":
+        #Vx = Cx
+        #Vy = Cy
+    #elif V == "D":
+        #Vx = Dx
+        #Vy = Dy
+    #elif V == "V":
+        #typeOfInput = 2
+    #else:
+        #print ("However, your choice of a vertice was neither A, B, C, D or V. Please try again.")
+        #typeOfInput = 3
+    #if typeOfInput == 1:
+        #finalDistance = pointsDistance(Px, Py, Vx, Vy)
+        #print ("The point of your choice (%i,%i), is %0.2f units away from vertice %s." %(Px, Py, finalDistance, V))
+    #elif typeOfInput == 2:
+        #closestVertice = findClosestVertice(Px, Py)
+        #print ("The point of your choice (%i,%i), is closest to vertice %s." %(Px, Py, closestVertice))
+#else:
+    #print ("Your coordinates dose not lie within this rectangle.")
+
+
+#Level 4+
+
+from math import *                                                                           #Imported all functions from math which some will be utilized later.
+def XYVSeperation(ans):                                                                      #Defining function "XYVSeperation", used in order to seperate the form (x, y)V
+    ans = ans.replace(" ","")                                                                #Takes out all the spaces within the input to simplify the process
+    if ans.count(",") and ans.count("(") and ans.count(")") and ans.find("(") == 0:          #Makes sure the input contains all the symbols within the correct input format. Otherwise it does not pass this point
+        x = ans[ans.find("(") + 1:ans.find(",")]                                             #Pulls the x-value out of the input, leaving any symbols stays with it
+        y = ans[ans.find(",") + 1:ans.find(")")]                                             #Pulls the y-value out of the input, leaving any symbols stays with it
+        xCompare = x.replace("-","").replace("+","")                                         #Removes any math symbols such as "-" and "+" from x
+        yCompare = y.replace("-","").replace("+","")                                         #Removes any math symbols such as "-" and "+" from y
+        if xCompare.isdigit() == True and yCompare.isdigit() == True:                                       #Checking if the inputs were infact digits and not letters or symbols
+            x = int(x)                                                                                      #Puts the value of x in to integar form
+            y = int(y)                                                                                      #Puts the value of y in to integar form
+            v = ans[ans.find(")") + 1::]                                                                    #Finds to see what the value of v is
+            if v.isalpha() == False:                                                                        #Now checks to see if v is infact a letter
+                print("Please make sure your V value is a letter and either A, B, C, D, or V. Please do not add more than 1 set of bracket!")             #Informs the user of their mistake
+            else:
+                if Ax <= x <= Cx and Ay <= y <= Cy:                                                         #Making sure the given x and y is within the rectangle
+                    typeOfInput = False
+                    print ("Your coordinates lies within this rectangle.")      #Indicating to the user that the position given is fine to proceed
+                    if v == "A":        #Checking if v is vertice A
+                        Vx = Ax
+                        Vy = Ay
+                    elif v == "B":      #Checking if v is vertice A
+                        Vx = Bx
+                        Vy = By
+                    elif v == "C":      #Checking if v is vertice A
+                        Vx = Cx
+                        Vy = Cy
+                    elif v == "D":      #Checking if v is vertice A
+                        Vx = Dx
+                        Vy = Dy
+                    elif v == "V":      #Checking if v is V
+                        typeOfInput = True
+                        print ("The point of your choice (%i,%i), is closest to vertice %s." %(x, y, findClosestVertice(x, y)))                     #Giving the user the output of the closest Vertice
+                    else:
+                        typeOfInput = True
+                        print ("However, your choice of a vertice was neither A, B, C, D or V (making sure it is caplized). Please try again.")     #Informing the user of their errors, asks to try again
+                    if typeOfInput == False:
+                        print ("The point of your choice (%i,%i), is %0.2f units away from vertice %s." %(x, y, pointsDistance(x, y, Vx, Vy), v))   #Giving the user the distance between their point and the vertice of choice
+                else:
+                    print ("Your coordinates does not lie within this rectangle.")  #Informing the user of they incorrect formating
+        else:
+            print ("Please input in the correct formating. (input x and y in terms of digits.)")                        #Informing the user of they incorrect formating
+    else:
+        print ("Please input in the format (x,y) V. Making sure to include brackets and commas.")                                #Informing the user of they incorrect formating
+def pointsDistance(x1, y1, x2, y2):                 #Function to find distance between two points
+    d = float(sqrt((x2 - x1)**2 + (y2 - y1)**2))    #Uses the formula to find out distance
+    return d
+def findClosestVertice(x1, y1):                 #Creating function to find the closest Vertice when V is given
+    minDistance = pointsDistance(x1, y1, Ax, Ay)
+    ans = "A"
+    if minDistance > pointsDistance(x1, y1, Bx, By):
+        minDistance = pointsDistance(x1, y1, Bx, By)
+        ans = "B"
+    if minDistance > pointsDistance(x1, y1, Cx, Cy):    
+        minDistance = pointsDistance(x1, y1, Cx, Cy)
+        ans = "C"        
+    if minDistance > pointsDistance(x1, y1, Dx, Dy):    
+        minDistance = pointsDistance(x1, y1, Dx, Dy)
+        ans = "D"
+    return ans                                  #Returns the shortest distance
+Ax = -5          #Asigning the point for A
+Ay = -5
+length = 17     #Assigning the length of rectangle
+width = 21      #Assigning the width of rectangle
+Bx = Ax + length
+By = Ay
+Cx = Ax + length
+Cy = Ay + width
+Dx = Ax
+Dy = Ay + width
+print ("The rectangle has corners A(%i,%i), B(%i,%i), C(%i,%i), D(%i,%i)." %(Ax,Ay,Bx,By,Cx,Cy,Dx,Dy))
+XYVSeperation(input("Please enter the X and Y coordinates of your point, followed by a vertices of your choice in the form (x,y) V \n(V being either \"A, B, C, D or V\". Inputting V will result in the return of the closest vertice to your point): "))                                                                       #Excecutes the main program after assigning the rectangle
